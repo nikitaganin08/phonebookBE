893af6192a001afa06b5560988b0dc7c07b1930e
ganin.n.a <ganin.n.a@sberbank.ru>
2021-07-15 21:44:50 +0300

    3.9 phonebook backend step9(backend with frontend)

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
-morgan.token('body', (req, res) => req.body.name ? JSON.stringify(req.body) : '')
 const app = express()
+const cors = require('cors')
 
+morgan.token('body', (req, res) => req.body.name ? JSON.stringify(req.body) : '')
 app.use(express.json())
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
@@ -53,7 +55,7 @@ app.delete('/api/persons/:id', ((req, res) => {
     const id = Number(req.params.id)
     persons = persons.filter(person => person.id !== id)
 
-    res.status(204)
+    res.status(204).end()
 }))
 
 app.post('/api/persons', ((req, res) => {

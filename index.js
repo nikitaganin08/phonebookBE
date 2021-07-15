e33ec834fc00d9da838554236d0b4e943e90896a
ganin.n.a <ganin.n.a@sberbank.ru>
2021-07-15 10:59:24 +0300

    3.8*: Phonebook backend step8(add request body to log)

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+morgan.token('body', (req, res) => req.body.name ? JSON.stringify(req.body) : '')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {

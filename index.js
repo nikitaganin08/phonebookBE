720b7a872d59e449c0c45e2e757e960c7e3d76f2
ganin.n.a <nikitaganin08@gmail.com>
2021-07-18 11:06:49 +0300

    3.13: Phonebook database, step1

--- index.js
+++ index.js
@@ -1,55 +1,40 @@
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
+const morgan = require('morgan')
 const cors = require('cors')
+require('dotenv').config()
+const Person = require('./models/person')
 
 app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
-morgan.token('body', (req, res) => req.body.name ? JSON.stringify(req.body) : '')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
+morgan.token('body', (req, res) => req.body.name ? JSON.stringify(req.body) : '')
+
 
 app.get('/api/persons', ((req, res) =>
-        res.json(persons)
+        Person.find({}).then(persons => {
+            res.json(persons)
+        })
 ))
 
 app.get('/info', ((req, res) =>
-        res.send(
-            `<div>Phonebook has info for ${persons.length} people</div>
+        Person.find({}).then(persons => {
+            res.send(
+                `<div>Phonebook has info for ${persons.length} people</div>
                    <div>${new Date().toString()}</div>`)
+        })
 ))
 
 app.get('/api/persons/:id', ((req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+    Person.findById(req.params.id).then(person => {
+        if (person) {
+            res.json(person)
+        } else {
+            res.status(404).end()
+        }
+    })
 }))
 
 app.delete('/api/persons/:id', ((req, res) => {
@@ -82,7 +67,7 @@ app.post('/api/persons', ((req, res) => {
     res.json(person)
 }))
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })

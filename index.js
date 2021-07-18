31e7fa15b2cfa8c3604f584cd0e5cc124fea5f23
ganin.n.a <nikitaganin08@gmail.com>
2021-07-18 23:13:04 +0300

    3.19: Phonebook database, step7(add model validation)

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({error: 'malformatted id'})
+    } else if(error.name === 'ValidationError') {
+        return response.status(400).send({error: error.message})
     }
 
     next(error)
@@ -53,7 +55,7 @@ app.delete('/api/persons/:id', (req, res, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
     if (!body.name || !body.number) {
         return res.status(404).json({
@@ -68,6 +70,7 @@ app.post('/api/persons', (req, res) => {
 
     person.save()
         .then(savedPerson => res.json(savedPerson))
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator');
 
 const url = process.env.MONGODB_URI
 
@@ -12,9 +13,11 @@ mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindA
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {type: String, required: true, unique: true, index: true},
+    number: {type: String, required: true}
 })
+
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
